e4c2fe645a76ef71c558868411077d57202524e4
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-24 22:22:44 +0800

    chore: update DVC configuration and ignore patterns
    - Disables DVC analytics for privacy
    - Adds *.py files to dvcignore to exclude Python files from DVC tracking
    - Updates cherries-init.sh script to reflect these changes

--- scripts/cherries-init.sh
+++ scripts/cherries-init.sh
@@ -11,6 +11,7 @@ PROJECT_NAME="$(basename "$git_root")"
 if [[ ! -d ".dvc/" ]]; then
   dvc init
 fi
+dvc config --project core.analytics false
 dvc config --project core.autostage true
 if ! dvc remote default; then
   url="$(
@@ -23,5 +24,18 @@ if ! dvc remote default; then
   dvc remote modify --local "default" user "$(rbw get --field "username" "DVC")"
   dvc remote modify --local "default" password "$(rbw get --field "password" "DVC")"
 fi
+
+cat <<- EOF > .dvcignore
+# -*- mode: ignore; -*-
+# Add patterns of files dvc should ignore, which could improve
+# the performance. Learn more at
+# https://dvc.org/doc/user-guide/dvcignore
+
+*.log
+*.log.gz
+*.log.jsonl
+*.log.jsonl.gz
+*.py
+EOF
 
 rclone mkdir "dvc:/$PROJECT_NAME/"
