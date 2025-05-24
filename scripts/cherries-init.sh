321d203017bd2a1fc185574c566a20bd92c76ca5
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-24 20:35:09 +0800

    fix: improve DVC initialization and remote setup
    - Remove redundant mise hook
    - Fix conditional logic for DVC initialization
    - Ensure remote directory creation with rclone
    - Force remote configuration update if exists

--- scripts/cherries-init.sh
+++ scripts/cherries-init.sh
@@ -8,17 +8,20 @@ cd "$git_root"
 
 PROJECT_NAME="$(basename "$git_root")"
 
-if [[ -d ".dvc/" ]]; then
-  # dvc init
-  dvc config --project core.autostage true
+if [[ ! -d ".dvc/" ]]; then
+  dvc init
+fi
+dvc config --project core.autostage true
+if ! dvc remote default; then
   url="$(
     rbw get --field "uris" "DVC" |
       head --lines="1"
   )"
   url="${url/#http/webdav}"
-  url="${url%%/}"
-  url="${url}/$PROJECT_NAME/"
-  dvc remote add --local --default "default" "$url"
+  url="${url%%/}/$PROJECT_NAME/"
+  dvc remote add --local --default --force "default" "$url"
   dvc remote modify --local "default" user "$(rbw get --field "username" "DVC")"
   dvc remote modify --local "default" password "$(rbw get --field "password" "DVC")"
 fi
+
+rclone mkdir "dvc:/$PROJECT_NAME/"
